7953db147dd2856df1ebe2e6a6687066be6da97e
Nicola1971 <modxit@modx.it>
2015-02-19 20:36:37 +0100

    added img-thumbnail class
    added img-thumbnail class

--- assets/tvs/multitv/configs/swipeboxmg.config.inc.php
+++ assets/tvs/multitv/configs/swipeboxmg.config.inc.php
@@ -19,7 +19,7 @@ $settings['templates'] = array(
 'outerTpl' => ' <div class="row margin-bottom-20">
 [+wrapper+]</div>',
 'rowTpl' => '<div class="col-md-3 col-sm-6 thumb">
-<a class="zoomthumbnail" rel="gallerysw" title="[+title+]" href="[+image+]"><img class="img-responsive" src="[!phpthumb? &input=`[+image+]` &options=`w=400,h=300,zc=TL`!]" alt="[+title+]" title="[+title+]" /></a>
+<a class="zoomthumbnail" rel="gallerysw" title="[+title+]" href="[+image+]"><img class="img-responsive img-thumbnail" src="[!phpthumb? &input=`[+image+]` &options=`w=400,h=300,zc=TL`!]" alt="[+title+]" title="[+title+]" /></a>
 </div>
 ')
 ?>
